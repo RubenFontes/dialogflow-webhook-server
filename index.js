af1c16035eb96c0fb213c6174d5edaedd55a66fc
RubenFontes <rubencfontes17@gmail.com>
2021-07-28 18:52:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+const app = express()
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+
+app.post('/dialogflow-fulfillment', (request, response) => {
+    dialogflowFulfillment(request, response)
+})
+
+app.listen(port, () => {
+    console.log(`Abrindo na porta: ${port}`)
+})
+
+const dialogflowFulfillment = (request, response) => {
+    const agent = new WebhookClient({request, response})
+
+    function intentName(agent){
+        agent.add("Olá, esta mensagem está vindo do server")
+    }
+
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", sayHello)
+    agent.handleRequest(intentMap)
+
+}
