e67f3ee00cc566c3b3b8a9abb20e78d46dbf9584
RubenFontes <rubencfontes17@gmail.com>
2021-08-13 01:45:55 -0300

    test

--- index.js
+++ index.js
@@ -19,36 +19,35 @@ app.listen(port, () => {
     console.log(`Abrindo na porta: ${port}`)
 })
 
-if (intentName == 'EnviarEmail') {
-    var nodemailer = require('nodemailer');
-    var Email = request.body.queryResult.paremeters['Email'];
-    var Mensagem = request.body.queryResult.paremeters['Mensagem'];
-    var transporte = nodemailer.createTransport({
-        service: 'Outlook', //servidor a ser usado
-        auth: {
-            user: process.env.user, // dizer qual o usuário
-            pass: process.env.pass // senha da conta
-        }
-    });
-    var email = {
-        from: process.env.user, // Quem enviou este e-mail
-        to: Email, // Quem receberá
-        subject: "Assunto", // Um assunto
-        html: Mensagem // O conteúdo do e-mail
-    };
-    transporte.sendMail(email, function (error, info) {
-        if (error)
-            console.log(error);
-        throw error; // algo de errado aconteceu.
-        console.log('Email enviado! Leia as informações adicionais: ' + info);
-    });
-}
-
 const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({ request, response })
 
     function saudacao(agent) {
-        agent.add("Olá, esta mensagem está vindo do server")
+        //agent.add("Olá, esta mensagem está vindo do server")
+        if (intentName == 'EnviarEmail') {
+            var nodemailer = require('nodemailer');
+            var Email = request.body.queryResult.paremeters['Email'];
+            var Mensagem = request.body.queryResult.paremeters['Mensagem'];
+            var transporte = nodemailer.createTransport({
+                service: 'Outlook', //servidor a ser usado
+                auth: {
+                    user: process.env.user, // dizer qual o usuário
+                    pass: process.env.pass // senha da conta
+                }
+            });
+            var email = {
+                from: process.env.user, // Quem enviou este e-mail
+                to: Email, // Quem receberá
+                subject: "Assunto", // Um assunto
+                html: Mensagem // O conteúdo do e-mail
+            };
+            transporte.sendMail(email, function (error, info) {
+                if (error)
+                    console.log(error);
+                throw error; // algo de errado aconteceu.
+                console.log('Email enviado! Leia as informações adicionais: ' + info);
+            });
+        }
     }
 
     let intentMap = new Map();
