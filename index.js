dc521d2bc0f1a77240b5f9355026e9fc84de3938
RubenFontes <rubencfontes17@gmail.com>
2021-08-13 00:52:48 -0300

    nodemailer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
+const express = require('express');
 const { WebhookClient } = require('dialogflow-fulfillment');
+const nodemailer = require('nodemailer');
+const { request } = require('express');
 
 const app = express()
 app.use(express.json())
@@ -16,6 +18,33 @@ app.post('/dialogflow-fulfillment', (request, response) => {
 app.listen(port, () => {
     console.log(`Abrindo na porta: ${port}`)
 })
+
+var intentName = request.body.queryResult.intent.displayName;
+if (intentName == EnviarEmail) {
+    var Email = request.body.queryResult.parameters['Email'];
+    var Mensagem = request.body.queryResult.parameters['Mensagem'];
+    var nodemailer = request('nodemailer');
+    var transporte = nodemailer.createTransport({
+        service: 'Outlook',
+        auth: {
+            user: process.env.user,
+            pass: process.env.pass
+        }
+    })
+    var email = {
+        from: process.env.user,
+        to: Email,
+        subject: "Suporte de cliente",
+        html: Mensagem
+    };
+    transporte.sendMail(email, function (error, info) {
+        if (error)
+            console.log(error);
+        throw error;
+        console.log('Email enviado! Leia as informações adicionais' + info);
+    });
+}
+
 
 const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({ request, response })
