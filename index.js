9106a1b6c4a155ec7d84d8b7841760cbb0738744
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2022-04-19 21:15:08 -0300

    fix bugs

--- index.js
+++ index.js
@@ -44,60 +44,8 @@ const dialogflowFullfillment =(request, response) => {
                 //agent.add('Email enviado! Leia as informações adicionais: '+ info);
         });
     }
-    function envio_emailBackupdiferente(agent){
-        var nodemailer = require('nodemailer');
-        var sendmail = require('sendmail');
-        var transporter = nodemailer.createTransport({
-            sendmail: true,
-            newline: 'unix',
-            path: '/usr/sbin/sendmail',
-        });
-
-        var email = {
-            from:request.body.queryResult.parameters['email'], // Quem enviou este e-mail
-            to:"suporte@virtual.ufpb.br", // Quem receberá
-            subject: "Chamado de backup #dorinha", // Um assunto
-            html:"Abertura de chamado com assunto: BACKUP DE TURMAS.\n Informações fornecidas pelo usuário:\n NOME DO PROFESSOR:"+request.body.queryResult.parameters['nome']+"\nLOGIN DO USUÁRIO:"+ request.body.queryResult.parameters['login']+"\nNOME COMPLETO DA DISCIPLINA DE ORIGEM:"+ request.body.queryResult.parameters['nomedisciplinaorigem']+"\nNOME DISCIPLINA DE DESTINO:"+ request.body.queryResult.parameters['nomedisciplinadestino']
-        };
-        transporter.sendMail(email,function(error, info){ 
-                //console.log(info.envelope);
-                //console.log(info.messageId);
-                if(error){
-                    console.log (error);
-                    throw error; // algo de errado aconteceu.
-                }
-                //agent.add('Email enviado! Leia as informações adicionais: '+ info);
-        });
-    }
-    function envio_emailBackupigual(agent){
-        var nodemailer = require('nodemailer');
-        var sendmail = require('sendmail');
-        var transporter = nodemailer.createTransport({
-            sendmail: true,
-            newline: 'unix',
-            path: '/usr/sbin/sendmail',
-        });
-
-        var email = {
-            from:request.body.queryResult.parameters['email'], // Quem enviou este e-mail
-            to:"suporte@virtual.ufpb.br", // Quem receberá
-            subject: "Chamado de backup #dorinha", // Um assunto
-            html:"Abertura de chamado com assunto: BACKUP DE TURMAS.\n Informações fornecidas pelo usuário:\n NOME DO PROFESSOR:"+request.body.queryResult.parameters['nome']+"\nLOGIN DO USUÁRIO:"+ request.body.queryResult.parameters['login']+"\nNOME COMPLETO DA DISCIPLINA :"+ request.body.queryResult.parameters['nomedisciplina']
-        };
-        transporter.sendMail(email,function(error, info){ 
-                //console.log(info.envelope);
-                //console.log(info.messageId);
-                if(error){
-                    console.log (error);
-                    throw error; // algo de errado aconteceu.
-                }
-                //agent.add('Email enviado! Leia as informações adicionais: '+ info);
-        });
-    }
     
     let intentMap = new Map();
     intentMap.set("mail-ticketPadrao", envio_email)
-    intentMap.set("mail-backupTurmas - no", envio_emailBackupdiferente)
-    intentMap.set("mail-backupTurmas - SIM", envio_emailBackupigual)
     agent.handleRequest(intentMap)
 }
