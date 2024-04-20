32a7a041c4284a5db0328bcd2aefb7f9ad4ba5a4
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2024-04-20 19:51:07 -0300

    fiz nodemailler

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
 const express = require('express');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const nodemailer = require('nodemailer');
-const sendmail = require('sendmail');
 
 const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.post('/dialogflow-fullfillment', (request, response)=>{
-    dialogflowFullfillment(request, response);
+app.post('/dialogflow-fulfillment', (request, response)=>{
+    dialogflowFulfillment(request, response);
 });
 
 app.listen(port,() =>{
     console.log(`Listening on port ${port}`);
 });
 
-const dialogflowFullfillment = (request, response) => {
+const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({request, response})
-    function envio_email(agent){
+    function sendEmail(agent){
         var transporter = nodemailer.createTransport({
-            sendmail: true,
-            newline: 'unix',
-            path: '/usr/sbin/sendmail',
-          /*service: 'Outlook', //servidor a ser usado
+            service: 'Outlook', //servidor a ser usado
             auth: {
                 user: "email@hotmail.com", // dizer qual o usuário
                 pass: "Password123" // senha da conta
-            }*/
+            }
         });
 
         var email = {
-            from:request.body.queryResult.parameters['email'], // Quem enviou este e-mail
-            to:"suport@mail.com", // Quem receberá
+            from: request.body.queryResult.parameters['email'], // Quem enviou este e-mail
+            to: "suport@mail.com", // Quem receberá
             subject: "Chamado livre #dorinha", // Um assunto
             html: request.body.queryResult.parameters['mensagem'] // O conteúdo do e-mail
         };
-        transporter.sendMail(email,function(error, info){ 
-                //console.log(info.envelope);
-                //console.log(info.messageId);
-                if(error){
-                    console.log(error);
-                    throw error; 
-                }else{
-                    agent.add('Email enviado! Leia as informações adicionais: ' + info);
-                }
+
+        transporter.sendMail(email, function(error, info){ 
+            if(error){
+                console.log(error);
+                throw error; 
+            } else {
+                agent.add('Email enviado! Leia as informações adicionais: ' + info.response);
+            }
         });
     };
     
     let intentMap = new Map();
-    intentMap.set("mail-ticketPadrao", envio_email)
+    intentMap.set("mail-ticketPadrao", sendEmail)
     agent.handleRequest(intentMap)
 };
